7a1fcd21c251ac9170f94bc0c5da1f2cebe1eb4c
Lui H <tech.luiholl@gmail.com>
2022-07-12 21:07:31 +0200

    changes to find-longest + 2 additions, find-shortest

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -4,8 +4,10 @@ const findLongestString = require("./find-longest-string");
 test("findLongestString finds the longest string in an array", () => {
   expect(findLongestString(["it", "is", "a", "nice", "day"])).toBe("nice");
   expect(findLongestString(["why", "hello", "to", "you"])).toBe("hello");
+  expect(findLongestString(["Lui", "has", "got", "this"])).toBe("this");
 });
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["yeah", "he", "has", "this"])).toBe("yeah");
 });
